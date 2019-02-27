59d9c73b00b70fe272b57cbf0ee2847c3605eecc
stevechow <290689305@qq.com>
2019-02-27 16:38:52 +0800

    完成分类页面的开发

--- src/components/home/Category.vue
+++ src/components/home/Category.vue
@@ -28,12 +28,15 @@
 
     export default {
         data() {
-            return {
-                types: [],
-                allGoods: []
-            }
+            return {}
         },
         methods: {
+            getTypes() {
+
+            },
+            getAllGoods() {
+
+            },
             start() {
                 mui.init({
                     swipeBack: true //启用右滑关闭功能
@@ -45,143 +48,150 @@
                     j = 1,
                     m = 6, //左侧选项卡数量+1
                     n = 6; //每个选项卡列表数量+1
-                // for (; i < m; i++) {
-                //     html.push('<a class="mui-control-item" data-index="' + (i - 1) + '" href="#content' + i + '">选项' + i + '</a>');
-                // }
-                // html.push('<a class="mui-control-item" data-index="0">推荐</a>');
-                // html.push('<a class="mui-control-item" data-index="1">手机</a>');
-                // html.push('<a class="mui-control-item" data-index="2">电脑</a>');
-                // html.push('<a class="mui-control-item" data-index="3">生活</a>');
-                // html.push('<a class="mui-control-item" data-index="4">智能</a>');
-               console.dir(this.types)
-                for (var i = 0; i < 5; i++) {
-                    // console.log(this.types)
-                    html.push('<a class="mui-control-item" data-index="' + i + '">' + 1 + '</a>');
-                }
-                controls.innerHTML = html.join('');
-                html = [];
-                for (i = 1; i < m; i++) {
-                    html.push('<div id="content' + i + '" class="mui-control-content"><ul class="mui-table-view">');
-                    // for (j = 1; j < n; j++) {
-                    //     html.push('<li class="mui-table-view-cell">第' + i + '个选项卡子项-' + j + '</li>');
-                    // }
-                    html.push('<li class="mui-table-view-cell"><div class="boxer"><img src="/static/img/1.35f78c6.png" alt=""></div></li>');
-                    html.push('<li class="mui-table-view-cell"><div class="img"><img src="/static/img/1.ed0a304.png" alt=""></div><span>iPhone 4s</span></li>');
-                    html.push('<li class="mui-table-view-cell"><div class="img"><img src="/static/img/1.ed0a304.png" alt=""></div><span>iPhone 4s</span></li>');
-                    html.push('<li class="mui-table-view-cell"><div class="img"><img src="/static/img/1.ed0a304.png" alt=""></div><span>iPhone 4s</span></li>');
-                    html.push('<li class="mui-table-view-cell"><div class="img"><img src="/static/img/1.ed0a304.png" alt=""></div><span>iPhone 4s</span></li>');
-                    html.push('</ul></div>');
-                }
-                contents.innerHTML = html.join('');
-                //默认选中第一个
-                controls.querySelector('.mui-control-item').classList.add('mui-active');
-                //			contents.querySelector('.mui-control-content').classList.add('mui-active');
-                (function () {
-                    var controlsElem = document.getElementById("segmentedControls");
-                    var contentsElem = document.getElementById("segmentedControlContents");
-                    var controlListElem = controlsElem.querySelectorAll('.mui-control-item');
-                    var contentListElem = contentsElem.querySelectorAll('.mui-control-content');
-                    var controlWrapperElem = controlsElem.parentNode;
-                    var controlWrapperHeight = controlWrapperElem.offsetHeight;
-                    var controlMaxScroll = controlWrapperElem.scrollHeight - controlWrapperHeight;//左侧类别最大可滚动高度
-                    var maxScroll = contentsElem.scrollHeight - contentsElem.offsetHeight;//右侧内容最大可滚动高度
-                    var controlHeight = controlListElem[0].offsetHeight;//左侧类别每一项的高度
-                    var controlTops = []; //存储control的scrollTop值
-                    var contentTops = [0]; //存储content的scrollTop值
-                    var length = contentListElem.length;
-                    for (var i = 0; i < length; i++) {
-                        controlTops.push(controlListElem[i].offsetTop + controlHeight);
-                    }
-                    for (var i = 1; i < length; i++) {
-                        var offsetTop = contentListElem[i].offsetTop;
-                        if (offsetTop + 100 >= maxScroll) {
-                            var height = Math.max(offsetTop + 100 - maxScroll, 100);
-                            var totalHeight = 0;
-                            var heights = [];
-                            for (var j = i; j < length; j++) {
-                                var offsetHeight = contentListElem[j].offsetHeight;
-                                totalHeight += offsetHeight;
-                                heights.push(totalHeight);
-                            }
-                            for (var m = 0, len = heights.length; m < len; m++) {
-                                contentTops.push(parseInt(maxScroll - (height - heights[m] / totalHeight * height)));
-                            }
-                            break;
-                        } else {
-                            contentTops.push(parseInt(offsetTop));
-                        }
-                    }
-                    contentsElem.addEventListener('scroll', function () {
-                        var scrollTop = contentsElem.scrollTop;
-                        for (var i = 0; i < length; i++) {
-                            var offsetTop = contentTops[i];
-                            var offset = Math.abs(offsetTop - scrollTop);
-//						console.log("i:"+i+",scrollTop:"+scrollTop+",offsetTop:"+offsetTop+",offset:"+offset);
-                            if (scrollTop < offsetTop) {
-                                if (scrollTop >= maxScroll) {
-                                    onScroll(length - 1);
-                                } else {
-                                    onScroll(i - 1);
-                                }
-                                break;
-                            } else if (offset < 20) {
-                                onScroll(i);
-                                break;
-                            } else if (scrollTop >= maxScroll) {
-                                onScroll(length - 1);
-                                break;
-                            }
-                        }
-                    });
-                    var lastIndex = 0;
-                    //监听content滚动
-                    var onScroll = function (index) {
-                        if (lastIndex !== index) {
-                            lastIndex = index;
-                            var lastActiveElem = controlsElem.querySelector('.mui-active');
-                            lastActiveElem && (lastActiveElem.classList.remove('mui-active'));
-                            var currentElem = controlsElem.querySelector('.mui-control-item:nth-child(' + (index + 1) + ')');
-                            currentElem.classList.add('mui-active');
-                            //简单处理左侧分类滚动，要么滚动到底，要么滚动到顶
-                            var controlScrollTop = controlWrapperElem.scrollTop;
-                            if (controlScrollTop + controlWrapperHeight < controlTops[index]) {
-                                controlWrapperElem.scrollTop = controlMaxScroll;
-                            } else if (controlScrollTop > controlTops[index] - controlHeight) {
-                                controlWrapperElem.scrollTop = 0;
-                            }
-                        }
-                    };
-                    //滚动到指定content
-                    var scrollTo = function (index) {
-                        contentsElem.scrollTop = contentTops[index];
-                    };
-                    mui(controlsElem).on('tap', '.mui-control-item', function (e) {
-                        scrollTo(this.getAttribute('data-index'));
-                        return false;
-                    });
-                })();
-            },
-            getTypes() {
+
+                // getTypes
+                var types = []
                 this.$http.get('api/types').then(result => {
                     if (result.body.status === 1) {
                         var arr = result.body.message
-                        for(var i in arr){
-                            this.types.push(arr[i].type)
+                        for (var i in arr) {
+                            types.push(arr[i].type)
                         }
+
+                        // 使用 types
+                        for (var i = 0; i < 5; i++) {
+                            html.push('<a class="mui-control-item" data-index="' + i + '">' + types[i] + '</a>');
+                        }
+                        controls.innerHTML = html.join('');
+                        html = [];
+
+                        // getGoods
+                        this.$http.get('api/allGoods').then(result => {
+                            if (result.body.status === 1) {
+                                var allGoods = result.body.message
+
+
+                                // 使用 goods
+                                allGoods.sort((a, b) => {
+                                    return (a.type_id - b.type_id)
+                                })
+
+                                console.log(allGoods)
+
+                                for (i = 1; i < 26; i+=5) {
+                                    html.push('<div id="content' + i + '" class="mui-control-content"><ul class="mui-table-view">');
+                                    html.push('<li class="mui-table-view-cell"><div class="boxer"><a href="http://localhost:8080/#/good/'+allGoods[i-1]._id+'"><img src="' + allGoods[i-1].img_ads + '" alt=""></a></div></li>');
+                                    for (j = 0; j < 5; j++) {
+                                        html.push('<li class="mui-table-view-cell"><a href="http://localhost:8080/#/good/'+allGoods[i+j-1]._id+'"><div class="img"><img src="' + allGoods[i+j-1].img_banner + '" alt=""></div></a><span>' + allGoods[i+j-1].name + '</span></li>');
+                                    }
+                                    html.push('</ul></div>');
+                                }
+                                // for (var i = 0; i < 25; i+=5) {
+                                //     console.log('我是banner:' + goods[i])
+                                //     for (var j = 0; j < 5; j++) {
+                                //         console.log('我是内容：'+goods[i+j])
+                                //     }
+                                // }
+
+                                contents.innerHTML = html.join('');
+                                //默认选中第一个
+                                controls.querySelector('.mui-control-item').classList.add('mui-active');
+                                //			contents.querySelector('.mui-control-content').classList.add('mui-active');
+                                (function () {
+                                    var controlsElem = document.getElementById("segmentedControls");
+                                    var contentsElem = document.getElementById("segmentedControlContents");
+                                    var controlListElem = controlsElem.querySelectorAll('.mui-control-item');
+                                    var contentListElem = contentsElem.querySelectorAll('.mui-control-content');
+                                    var controlWrapperElem = controlsElem.parentNode;
+                                    var controlWrapperHeight = controlWrapperElem.offsetHeight;
+                                    var controlMaxScroll = controlWrapperElem.scrollHeight - controlWrapperHeight;//左侧类别最大可滚动高度
+                                    var maxScroll = contentsElem.scrollHeight - contentsElem.offsetHeight;//右侧内容最大可滚动高度
+                                    var controlHeight = controlListElem[0].offsetHeight;//左侧类别每一项的高度
+                                    var controlTops = []; //存储control的scrollTop值
+                                    var contentTops = [0]; //存储content的scrollTop值
+                                    var length = contentListElem.length;
+                                    for (var i = 0; i < length; i++) {
+                                        controlTops.push(controlListElem[i].offsetTop + controlHeight);
+                                    }
+                                    for (var i = 1; i < length; i++) {
+                                        var offsetTop = contentListElem[i].offsetTop;
+                                        if (offsetTop + 100 >= maxScroll) {
+                                            var height = Math.max(offsetTop + 100 - maxScroll, 100);
+                                            var totalHeight = 0;
+                                            var heights = [];
+                                            for (var j = i; j < length; j++) {
+                                                var offsetHeight = contentListElem[j].offsetHeight;
+                                                totalHeight += offsetHeight;
+                                                heights.push(totalHeight);
+                                            }
+                                            for (var m = 0, len = heights.length; m < len; m++) {
+                                                contentTops.push(parseInt(maxScroll - (height - heights[m] / totalHeight * height)));
+                                            }
+                                            break;
+                                        } else {
+                                            contentTops.push(parseInt(offsetTop));
+                                        }
+                                    }
+                                    contentsElem.addEventListener('scroll', function () {
+                                        var scrollTop = contentsElem.scrollTop;
+                                        for (var i = 0; i < length; i++) {
+                                            var offsetTop = contentTops[i];
+                                            var offset = Math.abs(offsetTop - scrollTop);
+//						console.log("i:"+i+",scrollTop:"+scrollTop+",offsetTop:"+offsetTop+",offset:"+offset);
+                                            if (scrollTop < offsetTop) {
+                                                if (scrollTop >= maxScroll) {
+                                                    onScroll(length - 1);
+                                                } else {
+                                                    onScroll(i - 1);
+                                                }
+                                                break;
+                                            } else if (offset < 20) {
+                                                onScroll(i);
+                                                break;
+                                            } else if (scrollTop >= maxScroll) {
+                                                onScroll(length - 1);
+                                                break;
+                                            }
+                                        }
+                                    });
+                                    var lastIndex = 0;
+                                    //监听content滚动
+                                    var onScroll = function (index) {
+                                        if (lastIndex !== index) {
+                                            lastIndex = index;
+                                            var lastActiveElem = controlsElem.querySelector('.mui-active');
+                                            lastActiveElem && (lastActiveElem.classList.remove('mui-active'));
+                                            var currentElem = controlsElem.querySelector('.mui-control-item:nth-child(' + (index + 1) + ')');
+                                            currentElem.classList.add('mui-active');
+                                            //简单处理左侧分类滚动，要么滚动到底，要么滚动到顶
+                                            var controlScrollTop = controlWrapperElem.scrollTop;
+                                            if (controlScrollTop + controlWrapperHeight < controlTops[index]) {
+                                                controlWrapperElem.scrollTop = controlMaxScroll;
+                                            } else if (controlScrollTop > controlTops[index] - controlHeight) {
+                                                controlWrapperElem.scrollTop = 0;
+                                            }
+                                        }
+                                    };
+                                    //滚动到指定content
+                                    var scrollTo = function (index) {
+                                        contentsElem.scrollTop = contentTops[index];
+                                    };
+                                    mui(controlsElem).on('tap', '.mui-control-item', function (e) {
+                                        scrollTo(this.getAttribute('data-index'));
+                                        return false;
+                                    });
+                                })();
+                            } else {
+                                Toast('获取商品列表失败')
+                            }
+                        })
                     } else {
                         Toast('获取商品信息失败')
                     }
                 })
+
+
             },
-            getAllGoods() {
-                this.$http.get('api/allGoods').then(result => {
-                    if (result.body.status === 1) {
-                        this.allGoods = result.body.message
-                    } else {
-                        Toast('获取商品列表失败')
-                    }
-                })
-            }
         },
         components: {
             bottomBar
@@ -189,10 +199,6 @@
         mounted() {
             this.start()
         },
-        created() {
-            this.getTypes()
-            this.getAllGoods()
-        }
     }
 </script>
 
