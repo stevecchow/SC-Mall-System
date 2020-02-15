f4db63300e504ad2f9c43c21c7701175fe89f743
stevechow <290689305@qq.com>
2020-02-15 21:05:09 +0800

    修改样式

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,51 +1,51 @@
 <template>
-    <div>
-        <homeHeader></homeHeader>
+  <div>
+    <homeHeader></homeHeader>
 
-        <!--中间层-->
-        <div class="content">
-            <router-view></router-view>
-        </div>
-
-        <!--底部栏-->
-        <bottomBar></bottomBar>
+    <!--中间层-->
+    <div class="content">
+      <router-view></router-view>
     </div>
+
+    <!--底部栏-->
+    <bottomBar></bottomBar>
+  </div>
 </template>
 
 <script>
-    // 导入 mui.js
-    import mui from '../../lib/mui/js/mui.min.js'
+// 导入 mui.js
 
-    import bottomBar from './BottomBar'
-    import homeHeader from '../subComponents/HomeHeader'
-    console.log('Home')
+import mui from '../../lib/mui/js/mui.min.js'
 
-    export default {
-        data() {
-            return {
-                type_id:0,
-            }
-        },
-        methods: {},
-        mounted() {
-            mui(".mui-scroll-wrapper").scroll({
-                deceleration: 0.0005
-            });
-        },
-        components: {
-            bottomBar,
-            homeHeader
-        }
+import bottomBar from './BottomBar'
+import homeHeader from '../subComponents/HomeHeader'
+
+export default {
+  data () {
+    return {
+      type_id: 0,
     }
+  },
+  methods: {},
+  mounted () {
+    mui(".mui-scroll-wrapper").scroll({
+      deceleration: 0.0005
+    });
+  },
+  components: {
+    bottomBar,
+    homeHeader
+  }
+}
 </script>
 
 <style lang="less" scoped>
-    * {
-        touch-action: pan-y;
-    }
+* {
+  touch-action: pan-y;
+}
 
-    .content {
-        margin-top: 2.5rem;
-        margin-bottom: 1.2rem;
-    }
+.content {
+  margin-top: 2.5rem;
+  margin-bottom: 1.2rem;
+}
 </style>
